44f93e11ba4a26788a16d4231850fd7c70478d4c
Ohohcakester <ohohgit@gmail.com>
2015-07-30 08:21:50 +0800

    Some reorganisation/documentation for weightoptimisation.py

--- weightoptimisation.py
+++ weightoptimisation.py
@@ -10,8 +10,13 @@ import dataselect
 import util
 import constants as const
 
-keys = ['Close', 'RatioClose', 'AvgClose', 'DiffCloseSign', 'RunningRatio']
+# We try to find the optimal linear combination of these parameters.
+# Edit this list to configure the parameters to use.
+# Make sure that these parameters have been defined by parameters.readFile() first.
+parametersToOptimise = ['Close', 'RatioClose', 'AvgClose', 'DiffCloseSign', 'RunningRatio']
 
+# Test multiple algorithms
+# The average score of the algorithms is used as the objective function.
 algosToTest = {
     'sts': similarity.tsdist('stsDistance'),
     'inf.norm': similarity.tsdist('inf.normDistance'),
@@ -26,55 +31,45 @@ algosToTest = {
     'dissim': similarity.tsdist('dissimDistance'),
 }
 
+# Test only sts (overwrites the previous algosToTest if not commented out)
 algosToTest = {
     'sts': similarity.tsdist('stsDistance'),
 }
 
-data = {}
-headers = []
 
+# This is the main function to call in weightoptimisation.py.
+# Call this to run weight optimisation.
 def optimise():
     testCases = testcollection.readTests()
     #grouping.redefineGroupingConditions(dataselect.conditionBreakHigh)
     #grouping.redefineGroupingConditions(dataselect.conditionDoubleTopsFiltered)
 
+    ### Objective Function.
+    ### An input for pyswarm.pso. Returns a float utility score from testing with a set of weights.
     def testWithWeights(weights):
-        global keys
-        arrLen = len(keys)
+        arrLen = len(parametersToOptimise)
         weightDict = {}
         for i in range(0,arrLen):
-            weightDict[keys[i]] = weights[i]
+            weightDict[parametersToOptimise[i]] = weights[i]
         result = testAlgorithmsForAverageScore(testCases, weightDict)
-        print(str(result) + ' <- ' + str(weights))
+        print(str(result) + ' <- ' + str(weights)) # print as we go.
         return (result - 1)*100
    
-    """ 
-    import time
-    print ('start:')
-    s = time.time()
-    testWithWeights([1,1,1,1,0])
-    testWithWeights([1,1,1,0,0.5])
-    testWithWeights([1,1,0,0,0.5])
-    testWithWeights([1,0,1,0,0.5])
-    testWithWeights([1,0,1,1,0.5])
-    e = time.time()
-    print('end')
-    print(e - s)
-    return"""
-    ub = [1]*5
-    lb = [0]*5
+    ### Uncomment this to test the running time of weight optimisation.
+    # testWeightOptimisationRunningTime(testWithWeights)
+
+    ub = [1]*5 # Upper bound = 1
+    lb = [0]*5 # Lower bound = 0
     xopt, fopt = pyswarm.pso(testWithWeights, lb, ub, maxiter=20)
     
-    print('Weights:')
+    # If we reach this point, it means the weight optimisation process is complete.
+    print('Final Weights:')
     print(xopt)
     print('Score = ' + str(fopt))
 
 
-
-
-
+# weightDict is a dictionary (parameterName => weight)
 def testAlgorithmsForAverageScore(testCases, weightDict):
-    global algosToTest, data, headers
     allResults = []
     weightDataFun = weightedData(weightDict)
 
@@ -85,15 +80,16 @@ def testAlgorithmsForAverageScore(testCases, weightDict):
             #print('    algo ' + key)
             algo = algosToTest[key]
             for target in testCases[companyName]:
-                result = testAlgoWeighted(algo, target, weightDataFun)
+                result = testAlgoWeighted(algo, target, weightDataFun, data)
                 if result != None:
                     allResults.append(result)
 
     averageResult = testalgos.computeAverageResult(allResults)
-    return averageResult[2]
-    return averageResult[0]*averageResult[1]
+    return averageResult[2]                     # Maximise the amount of return alone.
+    #return averageResult[0]*averageResult[1]   # Maximise the product of LpScore and Rankscore
 
 
+# Returns a function that computes the linear combination of the weights and values from weightDict.
 def weightedData(weightDict):
     def fun(data):
         length = len(data['Close']) #length of any key in dict.
@@ -107,8 +103,8 @@ def weightedData(weightDict):
     return fun
 
 
-def testAlgoWeighted(algo, target, weightDataFun):
-    global data
+# Tests an algorithm using a weighted set of data, and returns a result object.
+def testAlgoWeighted(algo, target, weightDataFun, data):
     dates = data['Date']
     groupsWeighted = grouping.groupUp(data, weightDataFun(data))
     groupsClose = grouping.groupUp(data, data['Close'])
@@ -146,3 +142,20 @@ def testAlgoWeighted(algo, target, weightDataFun):
     #totalRank /= len(results2) # normalize totalRank for equal weightage.
 
     return (lpScore/nResults, totalRank/nResults, money)
+
+
+# A test function to see how fast the weight optimisation function runs.
+# I used this when I was trying to optimise the performance of the weight optimisation process.
+def testWeightOptimisationRunningTime(testWithWeights):
+    import time
+    print ('start:')
+    s = time.time()
+    testWithWeights([1,1,1,1,0])
+    testWithWeights([1,1,1,0,0.5])
+    testWithWeights([1,1,0,0,0.5])
+    testWithWeights([1,0,1,0,0.5])
+    testWithWeights([1,0,1,1,0.5])
+    e = time.time()
+    print('end')
+    print(e - s)
+    return
